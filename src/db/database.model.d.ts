1800e087f7110169bd4edd576263b47e3e7ee811
Florian Weidinger <crasyhorse@gmx.net>
2025-05-25 16:21:38 +0000

    chore Extend model with new props

--- src/db/database.model.d.ts
+++ src/db/database.model.d.ts
@@ -1,9 +1,14 @@
 import { User } from '@/db/user.model';
-import { AuthData } from './auth-data.model';
+import { AuthData } from '@/db/auth-data.model';
+import { Insurance } from '@/db/insurance.model';
+import { Option } from '@/db/option.model';
+import { BookingDataRecord } from './booking-data-record';
 
 export interface Database {
     cars: [];
     users: User[];
     auth: AuthData[];
+    bookings: BookingDataRecord[];
+    insurance: Insurance[],
+    options: Option[];
 }
-
