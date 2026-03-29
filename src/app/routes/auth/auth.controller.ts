daeca368254bcf324f8305a0c6d940a04627bb46
Florian Weidinger <crasyhorse@gmx.net>
2026-03-29 19:09:38 +0000

    fix: Tighten TypeScript typing in AuthController

--- src/app/routes/auth/auth.controller.ts
+++ src/app/routes/auth/auth.controller.ts
@@ -1,11 +1,17 @@
 import { NextFunction, Response, Request, Router } from 'express';
 import { login, register } from '@/app/routes/auth/auth.service.js';
+import { RegisterInput } from '@/app/routes/auth/register-input.model';
+import { LoginInput } from '@/app/routes/auth/login-input.model';
 
 const router = Router();
 
 router.post(
     '/auth/register',
-    async (req: Request, res: Response, next: NextFunction) => {
+    async (
+        req: Request<unknown, unknown, { user: RegisterInput }>,
+        res: Response,
+        next: NextFunction
+    ) => {
         try {
             const user = await register(req.body.user);
             res.status(201).json(user);
@@ -17,7 +23,11 @@ router.post(
 
 router.post(
     '/auth/login',
-    async (req: Request, res: Response, next: NextFunction) => {
+    async (
+        req: Request<unknown, unknown, { user: LoginInput }>,
+        res: Response,
+        next: NextFunction
+    ) => {
         try {
             const user = await login(req.body.user);
             res.status(200).json(user);
@@ -28,4 +38,3 @@ router.post(
 );
 
 export default router;
-
