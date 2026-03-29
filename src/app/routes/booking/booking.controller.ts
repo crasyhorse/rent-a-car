1708e1cd8df5c5af3d395e6e9173c6d6f0f92993
Florian Weidinger <crasyhorse@gmx.net>
2026-03-29 19:06:30 +0000

    fix: Check if customerId in path and payload are the same

--- src/app/routes/booking/booking.controller.ts
+++ src/app/routes/booking/booking.controller.ts
@@ -1,14 +1,28 @@
 import { NextFunction, Response, Request, Router } from 'express';
 import authHandler from '@/app/middleware/authorizationHandler';
 import { executeBooking } from '@/app/routes/booking/booking.service.js';
+import { BookingDataInput } from '@/db/booking-data-input.model';
 
 const router = Router();
 
 router.post(
     '/bookings/:customerId',
     authHandler.required,
-    async (req: Request, res: Response, next: NextFunction) => {
+    async (
+        req: Request<{ customerId: string }, unknown, { data: BookingDataInput }>,
+        res: Response,
+        next: NextFunction
+    ) => {
         try {
+            if (req.params.customerId !== req.body.data?.userId) {
+                res.status(422).json({
+                    status: 422,
+                    message: 'Customer id in path does not match payload user id.'
+                });
+
+                return;
+            }
+
             const booking = await executeBooking(req.body.data);
             res.status(201).json(booking);
         } catch (error) {
