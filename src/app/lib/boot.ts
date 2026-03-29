98322d3bfb1413bd54d91d5ac1c3de1ff05d1e44
Florian Weidinger <crasyhorse@gmx.net>
2026-03-29 19:04:29 +0000

    fix: Add missing BookingController to app

--- src/app/lib/boot.ts
+++ src/app/lib/boot.ts
@@ -2,9 +2,14 @@ import { Router } from 'express';
 import MainController from '@/app/routes/main/main.controller.js';
 import CarController from '@/app/routes/cars/car.controller.js';
 import AuthController from '@/app/routes/auth/auth.controller.js';
+import BookingController from '@/app/routes/booking/booking.controller.js';
 
 const router = Router();
 
-const api = router.use(MainController).use(CarController).use(AuthController);
+const api = router
+    .use(MainController)
+    .use(CarController)
+    .use(AuthController)
+    .use(BookingController);
 
 export default router.use('/api/v1', api);
--- src/app/routes/booking/booking.controller.ts
+++ src/app/routes/booking/booking.controller.ts
@@ -17,3 +17,4 @@ router.post(
     }
 );
 
+export default router;
