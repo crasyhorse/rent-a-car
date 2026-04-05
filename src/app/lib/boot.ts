21821e45ed46be41e449f1a4afbc42bd25347c87
Florian Weidinger <crasyhorse@gmx.net>
2026-04-05 17:56:50 +0000

    fix: Fix corrupt imports

--- src/app/lib/boot.ts
+++ src/app/lib/boot.ts
@@ -1,8 +1,10 @@
+import AuthController from '@/app/routes/auth/auth.controller';
+import BookingController from '@/app/routes/booking/booking.controller';
+import CarController from '@/app/routes/cars/car.controller';
+import MainController from '@/app/routes/main/main.controller';
+import PaymentController from '@/app/routes/payment/payment.controller';
+import ProfileController from '@/app/routes/userprofile/userProfile.controller';
 import { Router } from 'express';
-import MainController from '@/app/routes/main/main.controller.js';
-import CarController from '@/app/routes/cars/car.controller.js';
-import AuthController from '@/app/routes/auth/auth.controller.js';
-import BookingController from '@/app/routes/booking/booking.controller.js';
 
 const router = Router();
 
@@ -10,6 +12,9 @@ const api = router
     .use(MainController)
     .use(CarController)
     .use(AuthController)
-    .use(BookingController);
+    .use(BookingController)
+    .use(ProfileController)
+    .use(PaymentController);
 
 export default router.use('/api/v1', api);
+
