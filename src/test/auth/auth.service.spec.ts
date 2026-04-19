33e6843efdc7047db12f6d831aacfbf3a09306e5
Florian Weidinger <crasyhorse@gmx.net>
2026-04-19 13:21:59 +0000

    fix: Refactored specs

--- src/test/auth/auth.service.spec.ts
+++ src/test/auth/auth.service.spec.ts
@@ -1,25 +1,75 @@
-import { describe, it, expect, beforeEach, afterEach } from 'vitest';
-import { login, register } from '@/app/routes/auth/auth.service';
-import { LoginInput } from '@/app/routes/auth/login-input.model';
-import { RegisterInput } from '@/app/routes/auth/register-input.model';
 import HttpException from '@/app/models/HttpException';
-import type { Database } from '@/db/database.model';
-import { writeDatabase, readDatabase } from '@/db/db';
+import * as AuthRepository from '@/app/routes/auth/auth.repository';
+import { login, register } from '@/app/routes/auth/auth.service';
+import { LoginInput } from '@/db/login-input.model';
+import { RegisterInput } from '@/db/register-input.model';
+import { User } from '@/db/user.model';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 
 describe('auth.service', () => {
+    let expectedUser: User;
+
+    afterEach(() => {
+        vi.resetAllMocks();
+        vi.restoreAllMocks();
+    });
+
+    beforeEach(() => {
+        expectedUser = {
+            id: '8dd01190-e7c4-44f5-bcff-739565d8ea5a',
+            email: 'danieldeskclerk@example.com',
+            firstName: 'Daniel',
+            lastName: 'Deskclerk',
+            dateOfBirth: '2000-01-13',
+            phone: '173-555-12345',
+            driverLicense: {
+                numberMasked: 'F352GGE4711',
+                country: 'Germany',
+                verified: true,
+                expiryDate: '2028-01-13'
+            },
+            address: {
+                street: 'Garrison Ave',
+                houseNumber: '54',
+                zipCode: 'NJ 07306',
+                locality: 'Jersey City'
+            }
+        } satisfies User;
+    });
+
+    afterEach(() => {
+        vi.resetAllMocks();
+        vi.restoreAllMocks();
+    });
+
     describe('login', () => {
         describe('returns', () => {
             it('an authenticated user with a jwt.', async () => {
+                // Arrange
                 const loginInput: LoginInput = {
                     email: 'danieldeskclerk@example.com',
                     password: 'password'
                 };
 
-                const user = await login(loginInput);
+                const getUserByEmailSpy = vi
+                    .spyOn(AuthRepository, 'getUserByEmail')
+                    .mockResolvedValue(expectedUser);
 
-                expect(user.email).toMatch('danieldeskclerk@example.com');
-                expect(user).toHaveProperty('token');
-                expect(user.token).toBeTypeOf('string');
+                const expectedPasswordHash =
+                    '$2a$10$3vHJMxWUCFcN/bjE4KSane1ui3bpDpuNYfxDvnZdOcFXD9zxtIc3m';
+                const getPasswordByIdSpy = vi
+                    .spyOn(AuthRepository, 'getPasswordById')
+                    .mockResolvedValue(expectedPasswordHash);
+
+                // Act
+                const response = await login(loginInput);
+
+                // Assert
+                expect(response.user.email).toBe('danieldeskclerk@example.com');
+                expect(response).toHaveProperty('token');
+                expect(response.token).toBeTypeOf('string');
+                expect(getUserByEmailSpy).toHaveResolved();
+                expect(getPasswordByIdSpy).toHaveResolved();
             });
         });
 
@@ -27,163 +77,217 @@ describe('auth.service', () => {
             const expected403Error = new HttpException(403, 'Login failed!');
 
             it('an HTTP 403 if the user does not exist.', async () => {
+                // Arrange
                 const loginInput: LoginInput = {
                     email: 'nonexistinguser@example.com',
                     password: 'password'
                 };
 
-                await expect(login(loginInput)).rejects.toThrowError(
+                const getUserByEmailSpy = vi
+                    .spyOn(AuthRepository, 'getUserByEmail')
+                    .mockResolvedValue(undefined);
+
+                // Assert
+                await expect(login(loginInput)).rejects.toThrow(
                     expected403Error
                 );
+                expect(getUserByEmailSpy).toHaveResolved();
             });
 
             it('an HTTP 403 if there is not password hash for the user.', async () => {
+                // Arrange
                 const loginInput: LoginInput = {
-                    email: 'missingauth@example.com',
+                    email: 'danieldeskclerk@example.com',
                     password: 'password'
                 };
 
-                await expect(login(loginInput)).rejects.toThrowError(
+                const getUserByEmailSpy = vi
+                    .spyOn(AuthRepository, 'getUserByEmail')
+                    .mockResolvedValue(expectedUser);
+
+                const expectedPasswordHash = undefined;
+
+                const getPasswordByIdSpy = vi
+                    .spyOn(AuthRepository, 'getPasswordById')
+                    .mockResolvedValue(expectedPasswordHash);
+
+                // Assert
+                await expect(login(loginInput)).rejects.toThrow(
                     expected403Error
                 );
+                expect(getUserByEmailSpy).toHaveResolved();
+                expect(getPasswordByIdSpy).toHaveResolved();
             });
 
             it('an HTTP 403 if the password is wrong.', async () => {
+                // Arrange
                 const loginInput: LoginInput = {
                     email: 'danieldeskclerk@example.com',
                     password: 'wrong-password'
                 };
 
-                await expect(login(loginInput)).rejects.toThrowError(
+                const getUserByEmailSpy = vi
+                    .spyOn(AuthRepository, 'getUserByEmail')
+                    .mockResolvedValue(expectedUser);
+
+                const expectedPasswordHash =
+                    '$2a$10$3vHJMxWUCFcN/bjE4KSane1ui3bpDpuNYfxDvnZdOcFXD9zxtIc3m';
+                const getPasswordByIdSpy = vi
+                    .spyOn(AuthRepository, 'getPasswordById')
+                    .mockResolvedValue(expectedPasswordHash);
+
+                // Assert
+                await expect(login(loginInput)).rejects.toThrow(
                     expected403Error
                 );
+                expect(getUserByEmailSpy).toHaveResolved();
+                expect(getPasswordByIdSpy).toHaveResolved();
             });
 
-            [
-                { field: 'email', message: 'Email cannot be blank' },
-                { field: 'password', message: 'Password cannot be blank' }
-            ].forEach(({ field, message }) => {
-                it(`a HTTP 422 if ${field} is empty.`, async () => {
-                    const loginInput: LoginInput = {
-                        email: 'nonexistinguser@example.com',
-                        password: 'password'
-                    };
+            it.for([
+                { field: 'email', message: 'Email cannot be blank.' },
+                { field: 'password', message: 'Password cannot be blank.' }
+            ])('a HTTP 422 if $field is empty.', async ({ field, message }) => {
+                const loginInput: LoginInput = {
+                    email: 'danieldeskclerk@example.com',
+                    password: 'password'
+                };
 
-                    // @ts-expect-error This syntax is used in tests only
-                    loginInput[field] = null;
+                // @ts-expect-error This syntax is used in tests only
+                loginInput[field] = null;
 
-                    const expected422Error = new HttpException(422, message);
+                const expected422Error = new HttpException(422, message);
 
-                    await expect(login(loginInput)).rejects.toThrowError(
-                        expected422Error
-                    );
-                });
+                await expect(login(loginInput)).rejects.toThrow(
+                    expected422Error
+                );
             });
         });
     });
 
     describe('register', () => {
+        let registerInput: RegisterInput;
+
+        beforeEach(() => {
+            registerInput = {
+                email: 'clairevoyant@example.com',
+                firstName: 'Claire',
+                lastName: 'Voyant',
+                dateOfBirth: '2004-06-27',
+                address: {
+                    street: 'Golden Ave',
+                    houseNumber: '724',
+                    zipCode: 'NJ 07049',
+                    locality: 'Secaucus'
+                },
+                password: 'password',
+                phone: '011-555-12345'
+            };
+        });
+
         describe('returns', () => {
-            let databaseContent: Database;
-
-            beforeEach(async () => {
-                const _databaseContent = await readDatabase();
-                databaseContent = { ..._databaseContent };
-            });
-
-            afterEach(async () => {
-                await writeDatabase(databaseContent);
-            });
-
             it('an active user.', async () => {
-                const registerInput: RegisterInput = {
-                    email: 'clairevoyant@example.com',
-                    firstName: 'Claire',
-                    lastName: 'Voyant',
-                    dateOfBirth: '2004-06-27',
-                    driversLicense: 'G453HIF0815',
-                    address: {
-                        street: 'Golden Ave',
-                        houseNumber: '724',
-                        zipCode: 'NJ 07049',
-                        locality: 'Secaucus'
-                    },
-                    password: 'password'
+                // Arrange
+                const getUserByEmailSpy = vi
+                    .spyOn(AuthRepository, 'getUserByEmail')
+                    .mockResolvedValue(undefined);
+
+                const mergedUser: User = {
+                    ...registerInput,
+                    id: '8dd01190-e7c4-44f5-bcff-739565d8ea5a'
                 };
 
-                const user = await register(registerInput);
+                const mergeUserSpy = vi
+                    .spyOn(AuthRepository, 'mergeUser')
+                    .mockResolvedValue(mergedUser);
 
-                expect(user.email).toMatch('clairevoyant@example.com');
-                expect(user).toHaveProperty('token');
-                expect(user.token).toBeTypeOf('string');
+                const createAuthDataSpy = vi
+                    .spyOn(AuthRepository, 'createAuthData')
+                    .mockResolvedValue(undefined);
+
+                // Act
+                const response = await register(registerInput);
+
+                // Assert
+                expect(response.user.email).toBe('clairevoyant@example.com');
+                expect(response).toHaveProperty('token');
+                expect(response.token).toBeTypeOf('string');
+                expect(getUserByEmailSpy).toHaveResolved();
+                expect(mergeUserSpy).toHaveResolved();
+                expect(createAuthDataSpy).toHaveResolved();
             });
         });
 
         describe('throws', () => {
             it('an HTTP 422 if a user with this email address does already exist.', async () => {
-                const registerInput: RegisterInput = {
-                    email: 'danieldeskclerk@example.com',
-                    firstName: 'Daniel',
-                    lastName: 'Deskclerk',
-                    dateOfBirth: '2000-01-13',
-                    driversLicense: 'F352GGE4711',
-                    address: {
-                        street: 'Garrison Ave',
-                        houseNumber: '54',
-                        zipCode: 'NJ 07306',
-                        locality: 'Jersey City'
-                    },
-                    password: 'password'
+                // Arrange
+                const mergedUser: User = {
+                    ...registerInput,
+                    id: '8dd01190-e7c4-44f5-bcff-739565d8ea5a'
                 };
+                const getUserByEmailSpy = vi
+                    .spyOn(AuthRepository, 'getUserByEmail')
+                    .mockResolvedValue(mergedUser);
+
+                registerInput.email = 'clairevoyant@example.com';
 
                 const expectedError = new HttpException(
                     422,
                     'A user with this email address does already exist.'
                 );
 
-                await expect(() =>
-                    register(registerInput)
-                ).rejects.toThrowError(expectedError);
+                await expect(() => register(registerInput)).rejects.toThrow(
+                    expectedError
+                );
+                expect(getUserByEmailSpy).toHaveResolved()
             });
 
-            [
+            it.for([
                 { field: 'email', message: 'Email cannot be blank.' },
                 { field: 'password', message: 'Password cannot be blank.' },
+                { field: 'firstName', message: 'First name cannot be blank.' },
+                { field: 'lastName', message: 'Last name cannot be blank.' },
                 {
                     field: 'dateOfBirth',
                     message: 'Date of birth cannot be blank.'
                 },
-                {
-                    field: 'driversLicense',
-                    message: 'Drivers license cannot be blank.'
-                }
-            ].forEach(({ field, message }) => {
-                it(`an HTTP 422 if ${field} is empty.`, async () => {
-                    const registerInput: RegisterInput = {
-                        email: 'unclesam@example.com',
-                        firstName: 'Uncle',
-                        lastName: 'Sam',
-                        dateOfBirth: '1987-07-24',
-                        driversLicense: 'J461HHF4712',
-                        address: {
-                            street: 'Park Ave',
-                            houseNumber: '617',
-                            zipCode: 'NJ 07030',
-                            locality: 'Hoboken'
-                        },
-                        password: 'password'
-                    };
-
+                { field: 'phone', message: 'Phone cannot be blank.' },
+                { field: 'address', message: 'Address cannot be blank.' }
+            ])(
+                'an HTTP 422 if $field is empty.',
+                async ({ field, message }) => {
                     // @ts-expect-error This syntax is used in tests only
                     registerInput[field] = null;
 
                     const expectedError = new HttpException(422, message);
 
-                    await expect(() =>
-                        register(registerInput)
-                    ).rejects.toThrowError(expectedError);
-                });
-            });
+                    await expect(() => register(registerInput)).rejects.toThrow(
+                        expectedError
+                    );
+                }
+            );
+
+            it.for([
+                { field: 'street', message: 'Street cannot be blank.' },
+                {
+                    field: 'houseNumber',
+                    message: 'House number cannot be blank.'
+                },
+                { field: 'zipCode', message: 'Zip code cannot be blank.' },
+                { field: 'locality', message: 'Locality cannot be blank.' }
+            ])(
+                'an HTTP 422 if $field is empty.',
+                async ({ field, message }) => {
+                    // @ts-expect-error This syntax is used in tests only
+                    registerInput.address[field] = null;
+
+                    const expectedError = new HttpException(422, message);
+
+                    await expect(() => register(registerInput)).rejects.toThrow(
+                        expectedError
+                    );
+                }
+            );
         });
     });
 });
